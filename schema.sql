0b575a69ee647a0a74aac9741b8da36a6d01e5a3
Gr3nda <Chappronkevin@hotmail.com>
2026-05-28 14:43:25 +0200

    created base files and basic tables for schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+
+
+CREATE TABLE schools (
+  id INT  auto_increment,
+  name VARCHAR(255) not null,
+  PRIMARY KEY (id)
+
+);  
+
+CREATE TABLE students (
+  id INT auto_increment,
+  name VARCHAR(255) not null,
+  school_id INT not null,
+  PRIMARY KEY (id),
+  FOREIGN KEY (school_id) REFERENCES schools(id)
+);
+
+
+CREATE TABLE sports (
+  id INT auto_increment,
+  name VARCHAR(255) not null,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE student_sports (
+  student_id INT not null,
+  sport_id INT not null,
+  PRIMARY KEY (student_id, sport_id),
+  FOREIGN KEY (student_id) REFERENCES students(id),
+  FOREIGN KEY (sport_id) REFERENCES sports(id)
+);
